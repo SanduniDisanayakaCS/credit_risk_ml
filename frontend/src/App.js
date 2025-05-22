e29d0897fc97a8cd51a5b3ce8375db0467874c00
Sanduni Disanayaka <sandunidisanayaka96@gmail.com>
2025-05-22 14:45:44 +0530

    🔧 Fixed axios header for JSON prediction

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -23,7 +23,12 @@ function App() {
 
   const handleSubmit = async () => {
     try {
-      const res = await axios.post("/api/predict", { features });
+      const res = await axios.post("https://creditriskml-production.up.railway.app/predict", 
+  { features },
+  { headers: { "Content-Type": "application/json" } }
+);
+
+
       setPrediction(res.data.credit_risk_prediction);
     } catch (err) {
       alert("Prediction failed. Check backend connection.");
